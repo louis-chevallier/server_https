f57cf558f267f82c121806258f74cf981c007c60
louis chevallier <louis.chevallier@gmail.com>
2024-07-24 17:23:06 +0200

    The first thing we do, lets kill all the lawyers. 		-- Wm. Shakespeare, Henry VI, Part IV

--- server.py
+++ server.py
@@ -124,12 +124,15 @@ class App:
             batcmd="nmap -sL 192.168.1.*"
             result = subprocess.check_output(batcmd, shell=True, text=True)
             result = result.split("\n")
+            EKOX(result)
             self.devices_connected.clear()
             for e in result :
                 #EKO()
                 for ee in tels :
                     if ee in e :
+                        EKON(e, ee)
                         ip = re.search("\((.*)\)", e).groups()[0]
+                        EKOX(ip)
                         try :
                             pp = subprocess.run("ping -c 1 %s" % ip, shell=True, text=True, check=True, timeout=15, capture_output=True).stdout
                             pp = pp.split("\n")
@@ -145,7 +148,8 @@ class App:
                 self.alarm(mode)
                 
             #EKOX(self.devices_connected)
-            time.sleep(60*10)
+            time.sleep(6)
+            #time.sleep(60*10)
             EKO()
 
     def info(self) :
