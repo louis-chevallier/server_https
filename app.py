90ca9bb01b1266e853534d75a19544f65d5821e2
louis chevallier <louis.chevallier@gmail.com>
2024-11-12 10:47:30 +0100

    Dont plan any hasty moves.  Youll be evicted soon anyway.

--- app.py
+++ app.py
@@ -112,19 +112,19 @@ class App(App0) :
             time.sleep(60*1)
             #EKO()
 
-	@cherrypy.expose
-	def heure(self, rep) :
-		now = datetime.now()
-		print(now.year, now.month, now.day, now.hour, now.minute, now.second)
-		d = { "an" : now.year,
-			  "mois" : now.month,
-			  "jour": now.day,
-			  "heure" : now.hour,
-			  "minute" : now.minute,
-			  "seconde" : now.second }
+    @cherrypy.expose
+    def heure(self, rep) :
+        now = datetime.now()
+        print(now.year, now.month, now.day, now.hour, now.minute, now.second)
+        d = { "an" : now.year,
+              "mois" : now.month,
+              "jour": now.day,
+              "heure" : now.hour,
+              "minute" : now.minute,
+              "seconde" : now.second }
         sd = json.dumps(d)
-		return sd
-	
+        return sd
+    
     def status(self, rep) :
         with open(os.path.join(rootDir, "index.html"), "r") as file :
             data = file.read()
